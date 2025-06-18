59fb6a31bb5c216a4f8dafe893b4212f1e75bca7
ValentinMecchia <valentinmecchia@gmail.com>
2025-06-18 18:51:17 -0300

    modified api.js

--- src/constants/api.js
+++ src/constants/api.js
@@ -1,3 +1,3 @@
 export const API_BASE_URL = process.env.NEXT_PUBLIC_VERCEL_URL
-  ? `https://${process.env.REACT_APP_VERCEL_URL}` // Vercel asigna esta variable automáticamente
+  ? `https://${process.env.VITE_VERCEL_URL}` // Vercel asigna esta variable automáticamente
   : 'http://localhost:3000'; // Para desarrollo local
