39cfeda2060565411cded8099e8e55b1ceb57de9
ValentinMecchia <valentinmecchia@gmail.com>
2025-06-18 18:58:56 -0300

    modified api.js

--- src/constants/api.js
+++ src/constants/api.js
@@ -1,3 +1,6 @@
 export const API_BASE_URL = process.env.VITE_VERCEL_URL
   ? `https://${process.env.VITE_VERCEL_URL}`
   : 'http://localhost:3000';
+
+console.log("Valor de VITE_VERCEL_URL en runtime:", process.env.VITE_VERCEL_URL);
+console.log("Valor de API_BASE_URL calculado:", API_BASE_URL);
