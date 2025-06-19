3132d02bd9800ad5bd7c37b845d6a8e1cf4f534a
ValentinMecchia <valentinmecchia@gmail.com>
2025-06-19 15:32:51 -0300

    Solucionando redirect

--- src/constants/api.js
+++ src/constants/api.js
@@ -1,6 +1,1 @@
-export const API_BASE_URL = typeof window !== 'undefined'
-  ? window.location.origin
-  : 'http://localhost:3000';
-
-console.log("Valor de window.location.origin en runtime (frontend):", typeof window !== 'undefined' ? window.location.origin : 'N/A (no es navegador)');
-console.log("Valor de API_BASE_URL calculado (frontend):", API_BASE_URL);
+export const API_BASE_URL = import.meta.env.VITE_API_URL;
